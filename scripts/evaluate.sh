fd0a3cc1c7095a080f832e1802e81f4b90666b78
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-06-06 09:23:06 +0200

    remove unused data generation code

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 source $(dirname "$0")/setup.sh
 set -Eeuo pipefail
--- scripts/generateData.sh
+++ scripts/generateData.sh
@@ -1,29 +1,8 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 source $(dirname "$0")/setup.sh
 
 # This generates data
-
-
-#for len in (40 50 60 70); do
-#    mason_simulator -ir ${index} --illumina-read-length ${len} --out-alignment ${data}/reads/sampled_${len}.sam -o ${data}/reads/sampled_${len}.fasta --seq-technology illumina --num-fragments 2000000 --illumina-prob-mismatch=0.04 --illumina-prob-insert 0.0005 --illumina-prob-deletion 0.0005;
-#done
-#
-#for len in (80 90 100); do
-#    mason_simulator -ir ${index} --illumina-read-length ${len} --out-alignment ${data}/reads/sampled_${len}.sam -o ${data}/reads/sampled_${len}.fasta --seq-technology illumina --num-fragments 2000000 --illumina-prob-mismatch=0.02 --illumina-prob-insert 0.00025 --illumina-prob-deletion 0.00025;
-#done
-#
-#for len in (150 200); do
-#    mason_simulator -ir ${index} --illumina-read-length ${len} --out-alignment ${data}/reads/sampled_${len}.sam -o ${data}/reads/sampled_${len}.fasta --seq-technology illumina --num-fragments 2000000 --illumina-prob-mismatch=0.01 --illumina-prob-insert 0.000125 --illumina-prob-deletion 0.000125;
-#done
-#
-#
-#for len in 40 50 60 70 80 90 100 150 200; do
-#    for e in 0 1 2 3 4; do
-#        st_sam_filter ${data}/reads/sampled_${len}.sam ${data}/reads/sampled_${len}_${e}.sam --fasta ${data}/reads/sampled_${len}_${e}.fasta --dna4 --num 100000 --error ${e} --maxErrors ${e}
-#    done
-#done
-
 
 for len in 50; do
     mason_simulator -ir ${index} --illumina-read-length ${len} --out-alignment ${data}/reads/simulated_${len}.sam -o ${data}/reads/simulated_${len}.fasta --seq-technology illumina --num-fragments 20000 --illumina-prob-mismatch=0.04 --illumina-prob-insert 0.0005 --illumina-prob-deletion 0.0005 &
